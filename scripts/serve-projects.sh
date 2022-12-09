5374f479f3f1ce13125cb92d4b438253ae7f3ed7
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-12-09 21:26:22 +0100

    cc-inputs-outputs: added final code

--- scripts/serve-projects.sh
+++ scripts/serve-projects.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-re='^[0-9]+$'
-if ! [[ $1 =~ $re ]]; then
-  echo "error: Chapter is not a number" >&2
-  exit 1
-fi
-
-if [ -z $2 ]; then
+if [ -z $1 ]; then
   echo "error: App Name not provided" >&2
   exit 1
 fi
 
-CHAPTER="chapter$1"
-APP_NAME=$2
+APP_NAME=$1
 
 cd "start" && nx serve "$APP_NAME" --port=4200 &
 cd "final" && nx serve "$APP_NAME" --port=4201 && kill $!
